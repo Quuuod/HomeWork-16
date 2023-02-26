c87bec8752c2b363c2822adecea0129d3b5419d2
Quuod <prizraklogan@gmail.com>
2023-02-26 21:05:57 +0300

    get dates and rates

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-//145
-const cache = {}
+// //145
+// const cache = {}
 
-const getCurrency = (date) => {
-    if (cache[date]) {
-        console.log('from cache')
-        return new Promise((resolve) => resolve(cache[date]))
-    } else {
-        console.log('from BE')
-        return fetch(`https://www.nbrb.by/api/exrates/rates/usd?parammode=2&ondate=${date}`)
-            .then(res => res.json())
-            .then(currency => {
-                cache[date] = currency.Cur_OfficialRate
-                return currency.Cur_OfficialRate
-            })
-    }
+// const getCurrency = (date) => {
+//     if (cache[date]) {
+//         console.log('from cache')
+//         return new Promise((resolve) => resolve(cache[date]))
+//     } else {
+//         console.log('from BE')
+//         return fetch(`https://www.nbrb.by/api/exrates/rates/usd?parammode=2&ondate=${date}`)
+//             .then(res => res.json())
+//             .then(currency => {
+//                 cache[date] = currency.Cur_OfficialRate
+//                 return currency.Cur_OfficialRate
+//             })
+//     }
 
-}
-getCurrency('12-12-2021').then(console.log)
+// }
+// getCurrency('12-12-2021').then(console.log)
 
-getCurrency('12-12-2021').then(console.log)
+// getCurrency('12-12-2021').then(console.log)
 
 // document.querySelector('button').addEventListener('click', () => {
 //     getCurrency('12-12-2021').then(console.log)
@@ -45,13 +45,13 @@ const endDate = document.querySelector('#endDate')
 endDate.setAttribute('max', today)
 endDate.value = today
 
-function setDates(e){
+function setDates(e) {
     startDate.setAttribute('max', endDate.value)
 
-    if(startDate.value > endDate.value){
-    startDate.value = endDate.value
+    if (startDate.value > endDate.value) {
+        startDate.value = endDate.value
     }
-    if(startDate.value === ''){
+    if (startDate.value === '') {
         startDate.value = endDate.value
     }
 }
@@ -61,4 +61,24 @@ endDate.addEventListener('change', setDates)
 
 
 const btnSubmit = document.querySelector('#submit')
-console.log(btnSubmit)
+
+btnSubmit.addEventListener('click', e => {
+    const start = Date.parse(startDate.value)
+    const end = Date.parse(endDate.value)
+    const datesArray = []
+    const result = {}
+
+    for (let i = start; i <= end; i = i + 24 * 60 * 60 * 1000) {
+        datesArray.push(new Date(i).toISOString().slice(0, 10))
+    }
+
+    let requests = datesArray.map(date => fetch(`https://www.nbrb.by/api/exrates/rates/usd?parammode=2&ondate=${date}`));
+
+    Promise.all(requests)
+        .then(responses => responses)
+        .then(responses => Promise.all(responses.map(r => r.json())))
+        .then(currencies => currencies.forEach(currency => result[currency.Date.slice(0, 10)] = currency.Cur_OfficialRate));
+
+    console.log(result)
+})
+
